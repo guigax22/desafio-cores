c9812031e35ed29edb3c2e934699ba83ae494e4a
guigax22 <gs164994@gmail.com>
2022-05-23 22:55:35 -0300

    first commit

---
+++ cores/script.js
@@ -0,0 +1,20 @@
+let botao=document.querySelector('button');
+let fundo=document.querySelector('#corpo');
+let text=document.querySelector('h1');
+
+
+function change(){
+    text.innerHTML="Background #F2F2F2";
+    fundo.style.background='#F2F2F2';
+}
+
+    botao.addEventListener('click',change);
+
+
+  
+
+
+
+
+
+
